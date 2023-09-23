afbc53caa5d44a3474333fecf03b290d8edc1328
khian <khian@khiannecert19>
2023-09-23 12:58:29 +0800

    ACTIVITY 2.1212222quiz

---
+++ src/certifico_quiz_2/Certifico_Quiz_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package certifico_quiz_2;
+
+/**
+ *
+ * @author khian
+ */
+public class Certifico_Quiz_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
